6c118ea9489d9af0de79e7985a37399cf97de468
Ivan <ivan.merchep@gmail.com>
2021-09-29 03:47:21 +0200

    major changes

--- scripts/meson_setup.sh
+++ scripts/meson_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../
-meson setup build --buildtype=release --optimization=3 --native-file ./meson.ini
-# meson setup build/debug --buildtype=debug
+meson setup build --wipe --optimization=3 --native-file ./meson.ini
+# meson setup build/debug --buildtype=debug --native-file ./meson.ini
